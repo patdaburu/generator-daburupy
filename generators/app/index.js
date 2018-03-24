a1aa925643738780f281f92002a6fdd4e122c35a
patdaburu <pat@daburu.net>
2018-03-24 08:58:57 -0500

    Removed redundant 'make' in final yosay.

--- generators/app/index.js
+++ generators/app/index.js
@@ -176,6 +176,6 @@ module.exports = class extends Generator {
     this.log(`${chalk.green('make venv')}`);
     this.log(`${chalk.green('source venv/bin/activate')}`);
     this.log(`${chalk.green('make install')}`);
-    this.log(`${chalk.green('make make build')}`);
+    this.log(`${chalk.green('make build')}`);
   }
 };
